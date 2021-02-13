e729c70ea3b6cbef087a6a54634ce34c1974c2be
Sivert Schou Olsen <sivertschou@hotmail.com>
2021-02-13 15:20:38 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
-  Box,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    {/* <Box textAlign="center" fontSize="xl"> */}
+    {/* <Grid minH="100vh" p={3}> */}
+    <ColorModeSwitcher justifySelf="flex-end" />
+    <Tabs colorScheme="orange" isFitted>
+      <TabList>
+        <Tab>Run</Tab>
+        <Tab isDisabled>Ride</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>
+          <Text>Runs</Text>
+        </TabPanel>
+        <TabPanel>
+          <Text>Ride</Text>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+    <Text>Stravaknugen</Text>
+
+    {/* </Grid> */}
+    {/* </Box> */}
   </ChakraProvider>
-)
+);
