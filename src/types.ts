3bdd79d9d26aff289e1ee81ce2f0ef9de2a76dea
Sivert Schou Olsen <sivertschou@hotmail.com>
2023-03-13 22:44:09 +0100

    Remove segment types

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,3 @@
-export interface SegmentEffort {
-  name: string;
-  profile: string;
-  date: string;
-  effort: string;
-  pace: string;
-  bpm: string;
-  duration: string;
-  localRank?: number;
-}
-
 export interface InvitationalEffort {
   name: string;
   profile: string;
@@ -26,14 +15,6 @@ export interface InvitationalEffortFromApi {
   localRank?: number;
 }
 
-export interface Segment {
-  name: string;
-  shortName: string;
-  initials: string;
-  id: string;
-  type: 'run' | 'ride';
-}
-
 export interface Invitational {
   name: string;
   shortName: string;
@@ -49,12 +30,10 @@ export interface Club {
   name: string;
   linkName: string;
   id: string;
-  segments: Segment[];
 }
 
 export interface ClubEffortsFromApi {
   club: Club;
-  segmentEfforts: { segment: Segment; efforts: SegmentEffort[] }[];
   invitationalEfforts: {
     invitational: Invitational;
     efforts: InvitationalEffortFromApi[];
@@ -63,7 +42,6 @@ export interface ClubEffortsFromApi {
 
 export interface ClubEfforts {
   club: Club;
-  segmentEfforts: { segment: Segment; efforts: SegmentEffort[] }[];
   invitationalEfforts: InvitationalEffortGroup[];
 }
 
@@ -79,24 +57,9 @@ export interface Event {
   stravaLink?: string;
 }
 
-export interface LeaderboardSegmentEffort {
-  points: number;
-  effort: SegmentEffort;
-}
-
 export interface LeaderboardInvitationalEffort {
   points: number;
   effort: InvitationalEffort;
-}
-
-export interface SegmentAthlete {
-  name: string;
-  profile: string;
-  efforts: {
-    [segmentId: string]: LeaderboardSegmentEffort;
-  };
-  totalPoints: number;
-  rank: number;
 }
 
 export interface InvitationalAthlete {
