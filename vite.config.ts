1cc56ce3bb905dcbca200b4a42eb15f62c5c5448
Morten Kolstad <morten.kolstad@bekk.no>
2024-04-07 14:29:03 +0200

    add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
 });
