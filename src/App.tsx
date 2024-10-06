85e57225d231620f6ec54015131786440f31acf5
Morten Kolstad <morten.kolstad@bekk.no>
2024-10-06 22:14:24 +0200

    Add footer to toggle datamode

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,63 @@ import { BackgroundGraphics } from './BackgroundGraphics';
 import Beermile from './components/Event/Beermile/Beermile';
 import useEfforts from './hooks/efforts';
 import Admin from './components/Admin/Admin';
-import { Center, Loader, MantineProvider, Stack } from '@mantine/core';
+import { AppShell, Center, Footer, Loader, MantineProvider, Stack, Switch } from '@mantine/core';
+import React from 'react';
+import { DataModeContextProvider, useDataMode } from './hooks/DataModeContext';
 
 export const App = () => {
   const { efforts } = useEfforts();
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark', primaryColor: 'gray' }}>
-      {<BackgroundGraphics />}
-
-      <Stack>
-        <Center>
-          <Logo />
-        </Center>
-        {efforts ? (
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<InvitationalEffortTable allEfforts={efforts} />} />
-              <Route path="/beermile" element={<Beermile />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="*" element={<Navigate to={'/'} replace />} />
-            </Routes>
-          </BrowserRouter>
-        ) : (
-          <Center>
-            <Loader />
-          </Center>
-        )}
-      </Stack>
+      <DataModeContextProvider>
+        <AppShell footer={<DataModeFooter />}>
+          <BackgroundGraphics />
+          <Stack>
+            <Center>
+              <Logo />
+            </Center>
+            {efforts ? (
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<InvitationalEffortTable allEfforts={efforts} />} />
+                  <Route path="/beermile" element={<Beermile />} />
+                  <Route path="/admin" element={<Admin />} />
+                  <Route path="*" element={<Navigate to={'/'} replace />} />
+                </Routes>
+              </BrowserRouter>
+            ) : (
+              <Center>
+                <Loader />
+              </Center>
+            )}
+          </Stack>
+        </AppShell>
+      </DataModeContextProvider>
     </MantineProvider>
+  );
+};
+
+const DataModeFooter = () => {
+  const { dataMode, setDataMode, disabled } = useDataMode();
+  if (disabled) {
+    return null;
+  }
+  return (
+    <Footer height={50} p="md">
+      <Switch
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
+        checked={dataMode}
+        onChange={event => setDataMode(event.currentTarget.checked)}
+        size={'lg'}
+        onLabel="Pro"
+        offLabel="Basic"
+      />
+    </Footer>
   );
 };
