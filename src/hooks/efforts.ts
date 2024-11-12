3cc23fb795624567fe879979be5832d299222c57
Morten Kolstad <morten.kolstad@bekk.no>
2024-11-12 17:34:16 +0100

    test deploy

--- src/hooks/efforts.ts
+++ src/hooks/efforts.ts
@@ -47,7 +47,7 @@ const clubEfforts: ClubEffortsFromApi = {
   invitationalEfforts: [
     {
       invitational: {
-        name: 'Soria',
+        name: 'Soriaxx',
         shortName: 'Soria',
         initials: 'SO',
         id: 'Soria2024',
