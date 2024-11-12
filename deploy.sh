25213c9db1ea7bcb638b0ea9f5c9242fe59ba978
Morten Kolstad <morten.kolstad@bekk.no>
2024-11-12 19:25:55 +0100

    del

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 yarn --cwd ~/invitationals-frontend/ build
-cp -rf ~/invitationals-frontend/dist dist
+rm -rf *.{svg,js,ico,html,png,json,txt}
+cp -rf ~/invitationals-frontend/dist/* .
 git add -A
 git commit -m ${1:-"yiha"}
 git push
